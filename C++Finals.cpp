1d4c133b5f2ae3f7de5f256b5cf3a1294ee1baee
10007603 <10007603@ironschools.org>
2018-05-03 14:23:46 -0600

    Add files via upload

---
+++ C++Finals.cpp
@@ -0,0 +1,37 @@
+/*
+Jacob Palmer
+5/3/2018
+*/
+
+#include <iostream>
+#include <cstring>
+
+using namespace std;
+int main(int argc, char *argv[]) {
+	string year[12][1] = {Rat:2008_1996_1984_1972_1960},
+	{Ox:2009_1997_1985_1973_1961},
+	{Tiger:2010_1998_1986_1974_1962},
+	{Rabbit:2011_1999_1987_1975_1963},
+	{Dragon:2012_2000_1988_1976_1964},
+	{Snake:2013_2001_1989_1977_1965},
+	{Horse:2014_2002_1990_1978_1966},
+	{Goat:2015_2003_1991_1979_1967},
+	{Monkey:2016_2004_1992_1980_1968},
+	{Rooster:2017_2005_1993_1981_1969},
+	{Dog:2018_2006_1994_1982_1970},
+	{Pig:2019_2007_1995_1983_1971};
+	string yearBorn = {};
+	string age = {};
+	string month = {};
+	string season = {};
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}

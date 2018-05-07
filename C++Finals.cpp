b44408049f79c959d39701b340d8c19373ac2806
10007603 <10007603@ironschools.org>
2018-05-07 14:15:28 -0600

    Add files via upload

--- C++Finals.cpp
+++ C++Finals.cpp
@@ -8,22 +8,81 @@ Jacob Palmer
 
 using namespace std;
 int main(int argc, char *argv[]) {
-	string year[12][1] = {Rat:2008_1996_1984_1972_1960},
-	{Ox:2009_1997_1985_1973_1961},
-	{Tiger:2010_1998_1986_1974_1962},
-	{Rabbit:2011_1999_1987_1975_1963},
-	{Dragon:2012_2000_1988_1976_1964},
-	{Snake:2013_2001_1989_1977_1965},
-	{Horse:2014_2002_1990_1978_1966},
-	{Goat:2015_2003_1991_1979_1967},
-	{Monkey:2016_2004_1992_1980_1968},
-	{Rooster:2017_2005_1993_1981_1969},
-	{Dog:2018_2006_1994_1982_1970},
-	{Pig:2019_2007_1995_1983_1971};
-	string yearBorn = {};
-	string age = {};
-	string month = {};
-	string season = {};
+	int year[12][5] = {{2008, 1996, 1984, 1972, 1960},
+	{2009, 1997, 1985, 1973, 1961},
+	{2010, 1998, 1986, 1974, 1962},
+	{2011, 1999, 1987, 1975, 1963},
+	{2012, 2000, 1988, 1976, 1964},
+	{2013, 2001, 1989, 1977, 1965},
+	{2014, 2002, 1990, 1978, 1966},
+	{2015, 2003, 1991, 1979, 1967},
+	{2016, 2004, 1992, 1980, 1968},
+	{2017, 2005, 1993, 1981, 1969},
+	{2018, 2006, 1994, 1982, 1970},
+	{2019, 2007, 1995, 1983, 1971}};
+	int yearBorn = 0;
+	int yearZodiac = 0;
+	string rat = {People who are born in the year of the Rat are intelligent, charming, quick-witted, practical, ambitious, and good at economizing as well as social activities. The weaknesses are that the Rats are likely to be timid, stubborn, wordy, greedy, devious, too eager to get power and love to gossip.};
+	string ox = 1;
+	string tiger = 2;
+	string rabbit = 3;
+	string dragon = 4;
+	string snake = 5;
+	string horse = 6;
+	string goat = 7;
+	string monkey = 8;
+	string rooster = 9;
+	string dog = 10;
+	string pig = 11;
+	string zodiac = 0;	
+	
+	if (zodiac = 1){
+		for (int r = 0; r < 12; r ++){
+			for (int c = 0; c < 5; c ++){
+				if (int [r][c] = yearBorn){
+					yearZodiac = int r;
+				
+					if (yearZodiac == 0){
+						cout << rat << endl;
+					}
+					else if (yearZodiac == 1){
+						cout << ox << endl;
+					}
+					else if (yearZodiac == 2){
+						cout << tiger << endl;
+					}
+					else if (yearZodiac == 3){
+						cout << rabbit << endl;
+					}
+					else if (yearZodiac == 4){
+						cout << dragon << endl;
+					}
+					else if (yearZodiac == 5){
+						cout << snake << endl;
+					}
+					else if (yearZodiac == 6){
+						cout << horse << endl;
+					}
+					else if (yearZodiac == 7){
+						cout << goat << endl;
+					}
+					else if (yearZodiac == 8){
+					cout << monkey << endl;
+					}
+					else if (yearZodiac == 9){
+						cout << rooster << endl;
+					}
+					else if (yearZodiac == 10){
+						cout << dog << endl:
+					}
+					else if (yearZodiac == 11){
+						cout << pig << endl;
+					}			
+								
+				}	
+			}
+		}
+	}
 	
 	
 	
@@ -34,4 +93,10 @@ int main(int argc, char *argv[]) {
 	
 	
 	
+	
+	
+	
+	
+	
+	return 0;
 }
